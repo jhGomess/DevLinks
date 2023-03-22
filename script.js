5d160d6a29e75f49262839737a81b54120844f06
jhGomess <jhuangomes@hotmail.com>
2023-03-22 17:48:02 -0300

    adicionado uma imagem avatar-light na pasta assets e declarado as funcionalidades da função toggleMode no arquivo script.js.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function toggleMode() {
-  alert("oi")
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  // toggle é a simplificação da condição abaixo
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
