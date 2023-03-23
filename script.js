9352ad13d3fb557b9805129693d6b0b78d29d35f
jhGomess <jhuangomes@hotmail.com>
2023-03-23 14:05:57 -0300

    adicionado uma variavel no hover do footer e ajustes finais no javascript, colocando a alteração do alt da tag img.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle("light")
 
-  // toggle é a simplificação da condição abaixo
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
-
+  //pegar a tag img
   const img = document.querySelector("#profile img")
 
+  //substituir a imagem
   if (html.classList.contains("light")) {
+    //se estiver light-mode, adicionar essa imagem
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Jhuan Gomes sorrindo, usando oculos e utilizando uma camisa preta."
+    )
   } else {
+    // se estiver não estiver light-mode, adicionar essa imagem
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Jhuan Gomes em seu quarto, utilizando uma camisa preta."
+    )
   }
 }
